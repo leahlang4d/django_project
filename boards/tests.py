fb1c906bdb6b5986a3c1c1da9ec11505433a94b9
leahlang4d <leahlang4d@gmail.com>
2018-03-25 10:27:45 -0700

    tests

--- boards/tests.py
+++ boards/tests.py
@@ -2,3 +2,18 @@ from django.test import TestCase
 
 # Create your tests here.
 
+from django.urls import resolve,reverse
+from django.test import TestCase
+
+class HomeTest(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+#Simple test to  make sure the status code is returning the correct response
+
+    ###def test_home_url_resolves_home_view(self):
+       # view = resolve('/')
+        #self.assertEqual(view.func, home)
+#The above isn't working need to figure out what is happening
